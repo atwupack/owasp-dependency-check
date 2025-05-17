b2573211f93cba146587516112a18d98986fd5ce
André Twupack <atwupack@mailbox.org>
2025-05-17 18:24:57 +0200

    fixed eslint issues and added tests for ensureError

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,26 +1,54 @@
 import { describe, it } from "node:test";
 import * as assert from "node:assert/strict";
-import { hideSecrets } from "./utils.js";
+import { ensureError, hideSecrets } from "./utils.js";
 
-describe("hideSecrets", async () => {
-  it("should filter parameter with equal sign", async () => {
+void describe("hideSecrets", () => {
+  void it("should filter parameter with equal sign", () => {
     const result = hideSecrets("--nvdApiKey=1234567890");
     assert.equal(result, "--nvdApiKey=<secret value>");
   });
-  it("should filter parameter with space", () => {
+  void it("should filter parameter with space", () => {
     const result = hideSecrets("--nvdApiKey 1234567890");
     assert.equal(result, "--nvdApiKey <secret value>");
   });
-  it("should filter passwords", () => {
+  void it("should filter passwords", () => {
     const result = hideSecrets("--nvdPassword 1234567890");
     assert.equal(result, "--nvdPassword <secret value>");
   });
-  it("should filter tokens", () => {
+  void it("should filter tokens", () => {
     const result = hideSecrets("--nvdBearerToken 1234567890");
     assert.equal(result, "--nvdBearerToken <secret value>");
   });
-  it("should filter pass", () => {
+  void it("should filter pass", () => {
     const result = hideSecrets("--nexusPass 1234567890");
     assert.equal(result, "--nexusPass <secret value>");
+  });
+});
+
+void describe("ensureError", () => {
+  void it("should return the error", () => {
+    const error = new Error("Test Error");
+    assert.equal(ensureError(error), error);
+  });
+  void it("should convert a string to an error with the string in the message", () => {
+    const error = ensureError("Test Error");
+    assert.equal(
+      error.message,
+      'This value was thrown as is, not through an Error: "Test Error"',
+    );
+  });
+  void it("should convert an object to an error with the object in the message", () => {
+    const obj = {
+      message: "Test Error",
+      stack: "Test Stack",
+      name: "Test Name",
+      code: "Test Code",
+      errno: "Test Errno",
+    };
+    const error = ensureError(obj);
+    assert.equal(
+      error.message,
+      'This value was thrown as is, not through an Error: {"message":"Test Error","stack":"Test Stack","name":"Test Name","code":"Test Code","errno":"Test Errno"}',
+    );
   });
 });
