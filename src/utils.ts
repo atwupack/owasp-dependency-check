014e818a36dac540be6f250c27f55aad6338a2f2
André Twupack <atwupack@mailbox.org>
2025-06-19 22:19:35 +0200

    GH-59: switched color library to ansis

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
-import colors from "@colors/colors/safe.js";
 import extract from "extract-zip";
 import fs from "node:fs/promises";
+import { white} from "ansis";
 import { name } from "./info.js";
 
 export async function cleanDir(dir: string) {
@@ -20,18 +20,21 @@ async function deleteQuietly(path: string, recursive: boolean) {
 
 export function log(...logData: string[]) {
   console.log(
-    [colors.bgGreen(colors.white(` ${name}: `)), ...logData].join(" "),
+    [
+      white.bgGreen` ${name}: `,
+      ...logData,
+    ].join(" "),
   );
 }
 
 function logWarning(...logData: string[]) {
   console.log(
-    [colors.bgYellow(colors.white(" WARNING: ")), ...logData].join(" "),
+    [white.bgYellow` WARNING: `, ...logData].join(" "),
   );
 }
 
 export function logError(...logData: string[]) {
-  console.error([colors.bgRed(colors.white(" ERROR: ")), ...logData].join(" "));
+  console.error([white.bgRed` ERROR: `, ...logData].join(" "));
 }
 
 export function ensureError(value: unknown): Error {
