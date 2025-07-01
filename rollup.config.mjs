925d10bee4e46f4bc882a1472b36cb3dcda0e20e
André Twupack <atwupack@mailbox.org>
2025-07-01 23:23:30 +0200

    remove warnings during rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,5 +6,17 @@ export default {
     file: "dist/dependency-check.js",
     format: "es",
   },
+  external: [
+    "purify-ts",
+    "extract-zip",
+    "node:fs/promises",
+    "path",
+    "cross-spawn",
+    "ansis",
+    "fs",
+    "os",
+    "undici",
+    "@commander-js/extra-typings",
+  ],
   plugins: [terser()],
 };
